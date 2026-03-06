3ff71de6842d1a890af6a60c2116ff67a030ceed
RicardoAyala2003 <92557271+RicardoAyala2003@users.noreply.github.com>
2026-03-05 22:00:20 -0600

    commit cambios en header prueba
    wfk

--- header.php
+++ header.php
@@ -8,6 +8,6 @@
   <body <?php body_class(); ?>>
     <div class="bg-gray-300">
       <div class="max-w-4xl mx-auto mb-5 px-4">
-        <h1 class="text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-blue-500">Aqui se editan los Navbars</a></h1>
+        <h1 class="text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-blue-500">Aqui se editan los Navbarsasds</a></h1>
       </div>
     </div>
