bc5065c928e0754192b1879db89ef7c6d5744bb4
andrestdjhw <46946159+andrestdjhw@users.noreply.github.com>
2026-03-06 08:41:22 -0600

    Aplicando ejemplo de navbar tentativo para AJS Roofing and Gutters

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'a9b0cb4cdc485c5d3fcc');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '842863eefb06c263a5a0');
